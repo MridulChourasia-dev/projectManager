8a2f0959f8a8622a7c90b966de6cbc0510972f71
mridulchourasiya <mridulchourasia11@gmail.com>
2025-09-08 20:34:51 +0530

    create a email-verification

--- backend/libs/validate-schema.js
+++ backend/libs/validate-schema.js
@@ -10,4 +10,8 @@ const loginSchema = z.object({
   email: z.string().email("Invalid email address"),
   password: z.string().min(8, "Password must be at least 6 characters long"),
 });
-export { registerSchema, loginSchema };
+
+const varifyEmailSchema = z.object({
+  token: z.string().min(1, "Token is required"),
+});
+export { registerSchema, loginSchema, varifyEmailSchema };
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { z } from "zod";
+
 import { validateRequest } from "zod-express-middleware";
-import { loginSchema, registerSchema } from "../libs/validate-schema.js";
-import { loginUser, registerUser } from "../controllers/auth-controller.js";
+import {
+  loginSchema,
+  registerSchema,
+  varifyEmailSchema,
+} from "../libs/validate-schema.js";
+import { loginUser, registerUser, verifyEmail } from "../controllers/auth-controller.js";
+
 
 const router = express.Router();
 router.post(
@@ -18,6 +23,14 @@ router.post(
     body: loginSchema,
   }),
   loginUser
+);
+
+router.post(
+  "/verify-email",
+  validateRequest({
+    body: varifyEmailSchema,
+  }),
+  verifyEmail
 );
 
 export default router;
